e80cecb568898a8027fd984a4403404b5dd19e03
Balazs Racz <bracz@google.com>
2015-06-27 16:27:06 +0200

    updates all config.hxx to the new syntax.

--- io_board/webinar_app_6_cdi_config/targets/freertos.armv7m.ek-tm4c123gxl/config.hxx
+++ io_board/webinar_app_6_cdi_config/targets/freertos.armv7m.ek-tm4c123gxl/config.hxx
@@ -9,15 +9,15 @@ extern const SimpleNodeStaticValues SNIP_STATIC_DATA = {
 
 /// The main structure of the CDI. ConfigDef is the symbol we use in main.cxx
 /// to refer to the configuration defined here.
-BEGIN_GROUP(ConfigDef, base, MainCdi());
+CDI_GROUP(ConfigDef, MainCdi());
 /// Adds the <identification> tag with the values from SNIP_STATIC_DATA above.
-EXTEND_GROUP(ConfigDef, base, ident, Identification);
+CDI_GROUP_ENTRY(ident, Identification);
 /// Adds an <acdi> tag.
-EXTEND_GROUP(ConfigDef, ident, acdi, Acdi);
+CDI_GROUP_ENTRY(acdi, Acdi);
 /// Adds a segment for changing the values in the ACDI user-defined
 /// space. UserInfoSegment is defined in the system header.
-EXTEND_GROUP(ConfigDef, acdi, userinfo, UserInfoSegment);
-/// To end the group we have to declare what was the last entry.
-END_GROUP(ConfigDef, userinfo);
+CDI_GROUP_ENTRY(userinfo, UserInfoSegment);
+/// Closes the CDI declaration.
+CDI_GROUP_END();
 
 }  // namespace nmranet
--- io_board/webinar_app_8_gpio_pins/targets/freertos.armv7m.ek-tm4c123gxl/config.hxx
+++ io_board/webinar_app_8_gpio_pins/targets/freertos.armv7m.ek-tm4c123gxl/config.hxx
@@ -12,24 +12,22 @@ extern const SimpleNodeStaticValues SNIP_STATIC_DATA = {4, "OpenMRN",
 
 /// Defines the main segment in the configuration CDI. This is laid out at
 /// origin 128 to give space for the ACDI user data at the beginning.
-BEGIN_GROUP(
-    IoBoardSegment, base, Segment(MemoryConfigDefs::SPACE_CONFIG), Offset(128));
-EXTEND_GROUP(
-    IoBoardSegment, base, blinker, ConsumerConfig, Name("Blinker LED"));
-END_GROUP(IoBoardSegment, blinker);
+CDI_GROUP(IoBoardSegment, Segment(MemoryConfigDefs::SPACE_CONFIG), Offset(128));
+CDI_GROUP_ENTRY(blinker, ConsumerConfig, Name("Blinker LED"));
+CDI_GROUP_END();
 
 /// The main structure of the CDI. ConfigDef is the symbol we use in main.cxx
 /// to refer to the configuration defined here.
-BEGIN_GROUP(ConfigDef, base, MainCdi());
+CDI_GROUP(ConfigDef, MainCdi());
 /// Adds the <identification> tag with the values from SNIP_STATIC_DATA above.
-EXTEND_GROUP(ConfigDef, base, ident, Identification);
+CDI_GROUP_ENTRY(ident, Identification);
 /// Adds an <acdi> tag.
-EXTEND_GROUP(ConfigDef, ident, acdi, Acdi);
+CDI_GROUP_ENTRY(acdi, Acdi);
 /// Adds a segment for changing the values in the ACDI user-defined
 /// space. UserInfoSegment is defined in the system header.
-EXTEND_GROUP(ConfigDef, acdi, userinfo, UserInfoSegment);
-EXTEND_GROUP(ConfigDef, userinfo, seg, IoBoardSegment);
-/// To end the group we have to declare what was the last entry.
-END_GROUP(ConfigDef, seg);
+CDI_GROUP_ENTRY(userinfo, UserInfoSegment);
+CDI_GROUP_ENTRY(seg, IoBoardSegment);
+/// Closes the CDI declaration.
+CDI_GROUP_END();
 
 } // namespace nmranet
